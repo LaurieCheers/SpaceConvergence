d15c926bcda610b6027b125708abbd09e40cab7f
LaurieCheers <lauriecheers@gmail.com>
2016-10-19 09:46:31 +0100

    First commit

---
+++ SpaceConvergence/ConvergeCard.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LRCEngine;
+
+namespace SpaceConvergence
+{
+    [Flags]
+    public enum ConvergeCardType
+    {
+        Unit, // creature
+        Power, // land
+        Device, // artifact
+        Tech, // enchantment
+        Augment, // Aura
+        Planet, // planeswalker
+        Home, // player
+    };
+
+    public enum ConvergeZoneId
+    {
+        Hand,
+        Attack, // battlefield
+        Home, // battlefield
+        DiscardPile, // graveyard
+        Space, // exile
+    };
+    
+    
+    public class ConvergeZone
+    {
+        public ConvergePlayer owner;
+        public ConvergeZoneId zoneId;
+        public List<ConvergeObject> contents = new List<ConvergeObject>();
+        Vector2 basePos;
+        Vector2 slotOffset;
+
+        public ConvergeZone(JSONTable template, ConvergePlayer owner, ConvergeZoneId zoneId)
+        {
+            this.owner = owner;
+            this.zoneId = zoneId;
+            this.basePos = template.getVector2("basePos");
+            this.slotOffset = template.getVector2("slotOffset");
+        }
+
+        public void Add(ConvergeObject newObj)
+        {
+            if (newObj.zone != null)
+                newObj.zone.Remove(newObj);
+
+            newObj.slot = contents.Count;
+            newObj.zone = this;
+            contents.Add(newObj);
+        }
+
+        public void Remove(ConvergeObject oldObj)
+        {
+            contents.Remove(oldObj);
+            oldObj.zone = null;
+            oldObj.slot = 0;
+        }
+
+        public Vector2 GetNominalPos(int slot)
+        {
+            return basePos + slotOffset * slot;
+        }
+    }
+
+    public class ConvergePlayer
+    {
+        public ConvergeObject homeBase;
+        public ConvergeZone home;
+        public ConvergeZone attack;
+        public ConvergeZone hand;
+        public int life;
+
+        public ConvergePlayer(JSONTable template, ContentManager Content)
+        {
+            this.home = new ConvergeZone(template.getJSON("home"), this, ConvergeZoneId.Home);
+            this.attack = new ConvergeZone(template.getJSON("attack"), this, ConvergeZoneId.Attack);
+            this.hand = new ConvergeZone(template.getJSON("hand"), this, ConvergeZoneId.Hand);
+            this.homeBase = new ConvergeObject(new ConvergeCardSpec(template.getJSON("homebase"), Content), home);
+            this.life = template.getInt("startingLife");
+        }
+    }
+
+    public class ConvergeCardSpec
+    {
+        public readonly Texture2D art;
+        public readonly ConvergeCardType cardType;
+        public readonly int power;
+        public readonly int maxShields;
+
+        public ConvergeCardSpec(JSONTable template, ContentManager Content)
+        {
+            art = Content.Load<Texture2D>(template.getString("art"));
+            cardType = 0;
+            foreach (string name in template.getArray("cardType").asStrings())
+            {
+                cardType |= (ConvergeCardType)Enum.Parse(typeof(ConvergeCardType), name);
+            }
+            power = template.getInt("power", 0);
+            maxShields = template.getInt("shields", 0);
+        }
+    }
+
+    public class ConvergeObject
+    {
+        ConvergeCardSpec original;
+        public Texture2D art { get { return original.art; } }
+        public ConvergeCardType cardType { get { return original.cardType; } }
+        public int power { get { return original.power; } }
+        public int maxShields { get { return original.maxShields; } }
+        public ConvergeZone zone;
+        public int slot;
+
+        public int shields;
+        public int wounds;
+
+        public ConvergeObject(ConvergeCardSpec original, ConvergeZone zone)
+        {
+            this.original = original;
+            this.shields = maxShields;
+            this.wounds = 0;
+            zone.Add(this);
+        }
+
+        public void UseOn(ConvergeObject target)
+        {
+            int incomingDamage = target.power;
+            int dealtDamage = power;
+            TakeDamage(incomingDamage);
+            target.TakeDamage(dealtDamage);
+        }
+
+        void TakeDamage(int amount)
+        {
+            if(shields <= amount)
+            {
+                wounds += (amount - shields);
+                shields = 0;
+            }
+            else
+            {
+                shields -= amount;
+            }
+        }
+
+        public Rectangle nominalPosition
+        {
+            get { Vector2 pos = zone.GetNominalPos(slot); return new Rectangle((int)pos.X, (int)pos.Y, 50, 60); }
+        }
+    }
+
+    public class ConvergeUIObject : UIElement
+    {
+        ConvergeObject represented;
+        Rectangle gfxFrame;
+        bool isMouseOver;
+        bool isMousePressing;
+        bool isDragging;
+        Vector2 mousePressedPos;
+
+        public ConvergeUIObject(ConvergeObject represented)
+        {
+            this.represented = represented;
+            this.gfxFrame = represented.nominalPosition;
+        }
+
+        public override void Update(InputState inputState, Vector2 origin)
+        {
+            isMouseOver = (inputState.hoveringElement == this);
+            if (isMouseOver && inputState.mouseLeft.justPressed)
+            {
+                isMousePressing = true;
+                mousePressedPos = inputState.MousePos;
+            }
+
+            if (!inputState.mouseLeft.isDown)
+                isMousePressing = false;
+
+            float MINDRAG = 5;
+            float MINDRAGSQR = MINDRAG * MINDRAG;
+            if (isDragging)
+            {
+                if (isMousePressing)
+                {
+                    this.gfxFrame = new Rectangle((int)inputState.MousePos.X - 25, (int)inputState.MousePos.Y - 30, 50, 60);
+                }
+                else
+                {
+                    //dragged onto = inputState.hoveringElement
+                    if(inputState.hoveringElement != null)
+                    {
+                        if(inputState.hoveringElement is ConvergeUIObject)
+                        {
+                            represented.UseOn( ((ConvergeUIObject)inputState.hoveringElement).represented );
+                        }
+                    }
+
+                    this.gfxFrame = represented.nominalPosition;
+                    isDragging = false;
+                }
+            }
+            else if(isMousePressing && (inputState.MousePos - mousePressedPos).LengthSquared() > MINDRAGSQR)
+            {
+                isDragging = true;
+            }
+        }
+
+        public override UIMouseResponder GetMouseHover(Vector2 pos)
+        {
+            if (isDragging)
+                return null;
+
+            if (gfxFrame.Contains(pos))
+                return this;
+
+            return null;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 pos)
+        {
+            Texture2D art = represented.art;
+            spriteBatch.Draw(represented.art, new Rectangle(gfxFrame.Center.X - art.Width/2, gfxFrame.Bottom-art.Height, art.Width, art.Height), Color.White);
+
+            if (represented.cardType.HasFlag(ConvergeCardType.Unit))
+            {
+                if (represented.wounds > 0)
+                {
+                    spriteBatch.Draw(Game1.woundbg, new Rectangle(gfxFrame.Center.X - 11, gfxFrame.Center.Y, 22, 16), Color.White);
+                    spriteBatch.DrawString(Game1.font, "-" + represented.wounds, gfxFrame.Center.ToVector2(), TextAlignment.CENTER, Color.Red);
+                }
+                else
+                {
+                    if (represented.power > 0)
+                    {
+                        spriteBatch.Draw(Game1.powerbg, new Rectangle(gfxFrame.Left - 8, gfxFrame.Bottom - 10, 16, 16), Color.White);
+                        spriteBatch.DrawString(Game1.font, "" + represented.power, new Vector2(gfxFrame.Left, gfxFrame.Bottom - 10), TextAlignment.CENTER, Color.Yellow);
+                    }
+                    if (represented.shields > 0)
+                    {
+                        spriteBatch.Draw(Game1.shieldbg, new Rectangle(gfxFrame.Right - 8, gfxFrame.Bottom - 10, 16, 16), Color.White);
+                        spriteBatch.DrawString(Game1.font, "" + represented.shields, new Vector2(gfxFrame.Right, gfxFrame.Bottom - 10), TextAlignment.CENTER, represented.shields < represented.maxShields ? Color.Red : Color.White);
+                    }
+                }
+            }
+            
+            if(represented.cardType.HasFlag(ConvergeCardType.Home))
+            {
+                spriteBatch.DrawString(Game1.font, "" + represented.zone.owner.life, new Vector2(gfxFrame.Center.X, gfxFrame.Top + 20), TextAlignment.CENTER, Color.Black);
+            }
+
+            if (isMouseOver)
+            {
+                spriteBatch.Draw(Game1.mouseOverGlow, gfxFrame, Color.White);
+            }
+        }
+    }
+}
---
+++ SpaceConvergence/Game1.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.IO;
+using LRCEngine;
+
+namespace SpaceConvergence
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        JSONTable data;
+        List<ConvergeObject> hand;
+        UIContainer ui;
+        InputState inputState = new InputState();
+
+        ConvergePlayer self;
+        ConvergePlayer opponent;
+
+        public static SpriteFont font;
+        public static Texture2D powerbg;
+        public static Texture2D woundbg;
+        public static Texture2D shieldbg;
+
+        public static RichImage mouseOverGlow;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            data = new JSONTable("Content/data.json");
+
+            mouseOverGlow = new RichImage(data.getJSON("mouseOverGlow"), Content);
+
+            font = Content.Load<SpriteFont>("Arial");
+            shieldbg = Content.Load<Texture2D>("shieldbg");
+            powerbg = Content.Load<Texture2D>("powerbg");
+            woundbg = Content.Load<Texture2D>("woundbg");
+
+            self = new ConvergePlayer(data.getJSON("self"), Content);
+            opponent = new ConvergePlayer(data.getJSON("opponent"), Content);
+
+            ui = new UIContainer();
+
+            ui.Add(new ConvergeUIObject(self.homeBase));
+            ui.Add(new ConvergeUIObject(opponent.homeBase));
+
+            foreach(JSONTable cardTemplate in data.getArray("cards").asJSONTables())
+            {
+                ConvergeObject handCard = new ConvergeObject(new ConvergeCardSpec(cardTemplate, Content), self.hand);
+                ui.Add(new ConvergeUIObject(handCard));
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            inputState.Update();
+
+            inputState.hoveringElement = ui.GetMouseHover(inputState.MousePos);
+            ui.Update(inputState);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            ui.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
